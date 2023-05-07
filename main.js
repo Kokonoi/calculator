6d2abe8d47be6485148cafd1adced394afb06e42
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-05-07 13:52:47 +0200

    fixed some major things

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 let firstNum;
 let secondNum;
 let operator;
-let displayValue = 0;
+let displayValue = document.querySelector("div > .screen > #dpValue");
+console.log(displayValue.innerText);
+
+let btnNumbers = document.querySelectorAll(".numButton");
+btnNumbers.forEach((element) => {
+  element.addEventListener("click", () => {
+    displayValue.innerText = element.innerText;
+    firstNum = +displayValue.innerText;
+  });
+});
 
 function add(num1, num2) {
   return num1 + num2;
@@ -21,7 +30,7 @@ console.log(add(2, 3));
 function operate(a, b, op) {
   switch (op) {
     case "+":
-      add(a, b);
+      console.log(add(a, b));
       break;
     case "-":
       subtract(a, b);
