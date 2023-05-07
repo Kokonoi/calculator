4fabbc9792c535d8b4872845d051dc804eb2ace8
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-05-07 13:13:59 +0200

    made functions

---
+++ main.js
@@ -0,0 +1,38 @@
+let firstNum;
+let secondNum;
+let operator;
+let displayValue = 0;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(add(2, 3));
+
+function operate(a, b, op) {
+  switch (op) {
+    case "+":
+      add(a, b);
+      break;
+    case "-":
+      subtract(a, b);
+      break;
+    case "*":
+      multiply(a, b);
+      break;
+    case "/":
+      divide(a, b);
+      break;
+    default:
+      break;
+  }
+}
